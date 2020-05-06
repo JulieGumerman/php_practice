db388de2a689777b6a7f9a2599344daf9772c8d2
bodhibrdr <julie.gumerman@gmail.com>
2020-05-05 18:26:18 -0600

    login update shenanigans

---
+++ phpmyadmin/login_update.php
@@ -0,0 +1,66 @@
+
+<?php 
+include "db.php";
+include "utils.php"
+  
+
+
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <div class="col-sm-6">
+            <form
+                action="login-update.php"
+                method="post"
+                style="padding-top:30px;"
+            >
+                <div class="form-group">
+                    <label for="username">Username</label>
+                    <input 
+                        type="text" 
+                        name="username" 
+                        class="form-control"
+                    >
+                </div>
+                <div class="form-group">
+                    <label for="password">Password</label>
+                    <input 
+                        type="password" 
+                        name="password"
+                        class="form-control"
+                    >
+                </div>
+                <div class="form-group">
+                   <select name="id" id="">
+                   <?php 
+                    
+                        showAllData();
+                    
+                    ?>
+                   </select>
+                   
+
+                </div>
+                <input 
+                    class="btn btn-primary"
+                    type="submit" 
+                    name="submit" 
+                    value="UPDATE">
+            </form>
+
+                
+        </div>
+    </div>
+    
+</body>
