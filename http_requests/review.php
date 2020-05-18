c59d0f199ecaf3b4b178dff0ffadcddaca10b569
bodhibrdr <julie.gumerman@gmail.com>
2020-05-18 14:28:23 -0600

    review of global get, sessions, and cookies, round 1

---
+++ http_requests/review.php
@@ -0,0 +1,27 @@
+<?php 
+    session_start();
+    $_SESSION['value'] = "Crush it!";
+        
+
+    $name = "HarleyQuinn";
+    $value = "emancipation";
+    $expiration = time() + (60*60*24*7);
+    setcookie($name, $value, $expiration);
+
+    print_r($_GET);
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+   <?php 
+        $motto="Just because you are small does not mean you are insignificant";
+    ?>
+    <a href="review.php?motto=<?php echo $motto; ?>">CLICK ME!</a>
+</body>
+</html>
